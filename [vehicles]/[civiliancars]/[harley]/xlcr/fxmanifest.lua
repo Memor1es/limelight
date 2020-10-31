35df0ceaedccf5340bdce33b653a74d985f68b22
jorrit-erbium <jorrit@molenaarmail.nl>
2020-10-31 14:24:07 +0100

    harley

---
+++ [vehicles]/[civiliancars]/[harley]/xlcr/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'bodacious'
+game 'gta5'
+
+ -- Head over to https://docs.fivem.net/game-references/data-files/ for the data files
+ -- REMEMBER TO REMOVE THE FILE FROM files and data_file IF NOT IN RESOURCE!!
+ 
+files {
+    'data/vehicles.meta',
+	'data/carcols.meta',
+	'data/carvariations.meta',
+	'data/handling.meta',
+	'data/contentunlocks.meta',
+}
+
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlocks.meta'
